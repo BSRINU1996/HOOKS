1671081d1c56a1e7bb028dfbcd70bcef35103a6c
BSRINU1996 <balisrinu1996@gmail.com>
2022-09-21 11:21:32 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Counter from './useState';
-import User from './props'
+import User from './props';
+import Example from './useState2'
 function App() {
   return (
     <>
     < Counter/>
     <User age="25 "/>
+    <Example/>
     </>
   );
 }
